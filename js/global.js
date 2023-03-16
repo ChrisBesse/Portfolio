e421f85b5b47bdeeaa5f06de9f6e626f064a52e1
ChrisBesse <comteatedes@gmail.com>
2023-03-16 16:09:42 -0300

    Basic Javascript Done

---
+++ js/global.js
@@ -0,0 +1,28 @@
+// Changing Header properties when scrolling
+const header = document.querySelector(".header");
+
+window.onscroll = function () {
+  let top = window.scrollY;
+
+  if (top >= 100) {
+    header.classList.add("header--active");
+  } else {
+    header.classList.remove("header--active");
+  }
+};
+
+// Dark/Light Theme
+const DarkLightBtn = document.querySelector(".header__theme");
+
+DarkLightBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+// Sidebar
+
+const sidebar = document.querySelector(".header__links");
+const sidebarBtn = document.querySelector(".header__nav--toggle");
+
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("header__links--active");
+});
