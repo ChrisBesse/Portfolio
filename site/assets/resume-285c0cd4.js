3e52620b242d8d406c62f5e608b8e9e15ce95b69
ChrisBesse <comteatedes@gmail.com>
2023-06-13 20:18:35 -0300

    Updates to colors and resume

--- site/assets/resume-285c0cd4.js
+++
@@ -1,1 +0,0 @@
-import"./style-65755c4f.js";const e=""+new URL("Curriculo-pdf-f0f1b66b.pdf",import.meta.url).href;document.querySelector("#pdf-download").setAttribute("href",e);
---
+++ site/assets/resume-d1b346a6.js
@@ -0,0 +1,1 @@
+import"./style-e2664582.js";const e=""+new URL("Curriculo-pdf-222a719d.pdf",import.meta.url).href;document.querySelector("#pdf-download").setAttribute("href",e);
